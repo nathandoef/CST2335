76e852aad2fef27f99c967871d4eae31858027be
Nathan Doef <n.doef@live.com>
2017-11-24 21:17:30 -0500

    Finished Lab 6

---
+++ app/src/main/java/com/example/nathan/androidlabs/WeatherForecastActivity.java
@@ -0,0 +1,13 @@
+package com.example.nathan.androidlabs;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WeatherForecastActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather_forecast);
+    }
+}
